c3b7fbbc25f255bb4a894ed77812ac9f98b30343
William Rice <billyrice12@live.com>
2024-02-27 21:37:49 -0500

    initial state based todo working

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,96 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./components/ui/form";
+import { Input } from "./components/ui/input";
+
+interface Todo {
+  id: string;
+  text: string;
+  complete: boolean;
+}
+
+const formSchema = z.object({
+  text: z.string().min(2).max(50),
+});
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<Todo[] | []>([]);
+
+  const addTodo = (text: string) => {
+    const todo = {
+      id: crypto.randomUUID(),
+      text,
+      complete: false,
+    };
+
+    setTodos([...todos, todo]);
+  };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      text: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+
+    addTodo(values?.text);
+
+    form.reset();
+  }
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">count: {count}</h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setCount(count + 1)}
-      >
-        +
-      </button>
+      <div className="layout flex-col items-center mt-6 ">
+        <div className="container  max-w-fit">
+          <Form className="" {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 flex gap-1"
+            >
+              <FormField
+                control={form.control}
+                name="text"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Todo Text</FormLabel>
+                    <FormControl>
+                      <Input placeholder="" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+          <div>
+            {todos.map((todo) => (
+              <div key={todo.id}>
+                <span>
+                  {todo.id} : {todo.text}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
